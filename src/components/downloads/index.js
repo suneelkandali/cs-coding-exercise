1011d988c108b7eda9222f0d35c0d35aee1df863
Suneel Kandali <suneel.kandali@walmart.com>
2022-05-21 18:18:23 -0500

    checkboes select all/none changes checked in

--- src/components/downloads/index.js
+++ src/components/downloads/index.js
@@ -4,17 +4,65 @@ import { downloadsData } from "../../data/downloads";
 
 export const Downloads = () => {
   const [downloads, setDownloads] = useState([]);
+  const [isCheckAll, setIsCheckAll] = useState(false);
+  const [isCheck, setIsCheck] = useState([]);
+
+  const handleSelectAll = (e) => {
+    setIsCheckAll(!isCheckAll);
+    setIsCheck(downloads.map((item) => item.id));
+    if (isCheckAll) {
+      setIsCheck([]);
+    }
+  };
+
+  const handleClick = (e) => {
+    const { id, checked } = e.target;
+
+    if (!checked) {
+      setIsCheckAll(false);
+      setIsCheck(isCheck.filter((item) => item !== id));
+    } else {
+      setIsCheck([...isCheck, id]);
+      if (downloads.length === isCheck.length + 1) {
+        setIsCheckAll(true);
+      }
+    }
+  };
+
+  console.log(isCheck);
+
 
   useEffect(() => {
     setDownloads([...downloadsData]);
   }, [downloadsData]);
 
+  const Checkbox = ({ id, type, name, handleClick, isChecked }) => {
+    return (
+      <input
+        id={id}
+        name={name}
+        type={type}
+        onChange={handleClick}
+        checked={isChecked}
+      />
+    );
+  };
+
   const Download = ({ download }) => {
+    console.log(isCheck.includes(download.id));
+    console.log(download.id);
     return (
       <>
         <div class="selectArea">
           <span>
-            <input type="checkbox" />
+            <Checkbox
+              key={download.id}
+              type="checkbox"
+              name={download.name}
+              id={download.id}
+              handleClick={handleClick}
+              isChecked={isCheck.includes(download.id)}
+            />
           </span>
           <span class="cbLabel">Select</span>
         </div>
@@ -56,13 +104,25 @@ export const Downloads = () => {
       <div class="container">
         <div class="mobile">
           <span>
-            <input type="checkbox" />
+            <Checkbox
+              type="checkbox"
+              name="selectAll"
+              id="selectAll"
+              handleClick={handleSelectAll}
+              isChecked={isCheckAll}
+            />
           </span>
           <span>Selected 0 | ⤓ Download Selected</span>
         </div>
         <div class="grid">
           <div class="gridLabel">
-            <input type="checkbox" />
+            <Checkbox
+              type="checkbox"
+              name="selectAll"
+              id="selectAll"
+              handleClick={handleSelectAll}
+              isChecked={isCheckAll}
+            />
           </div>
           <div class="gridLabel">Selected 0</div>
           <div class="gridLabel">⤓ Download Selected</div>
--- src/data/downloads/index.js
+++ src/data/downloads/index.js
@@ -1,5 +1,6 @@
 export const downloadsData = [
     {
+        id: "1",
         name: "smss.exe",
         device: "Stark",
         path: "\\Device\\HarddiskVolume2\\Windows\\System32\\smss.exe",
@@ -7,6 +8,7 @@ export const downloadsData = [
     },
 
     {
+        id: "2",
         name: "netsh.exe",
         device: "Targaryen",
         path: "\\Device\\HarddiskVolume2\\Windows\\System32\\netsh.exe",
@@ -14,6 +16,7 @@ export const downloadsData = [
     },
 
     {
+        id: "3",
         name: "uxtheme.dll",
         device: "Lannister",
         path: "\\Device\\HarddiskVolume1\\Windows\\System32\\uxtheme.dll",
@@ -21,6 +24,7 @@ export const downloadsData = [
     },
 
     {
+        id: "4",
         name: "cryptbase.dll",
         device: "Martell",
         path: "\\Device\\HarddiskVolume1\\Windows\\System32\\cryptbase.dll",
@@ -28,6 +32,7 @@ export const downloadsData = [
     },
 
     {
+        id: "5",
         name: "7za.exe",
         device: "Baratheon",
         path: "\\Device\\HarddiskVolume1\\temp\\7za.exe",
